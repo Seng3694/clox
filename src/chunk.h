8be7d9638d13431685075f67a988c04e90be4f49
Seng <engelhardt_stefan@gmx.de>
2022-11-12 11:55:47 +0100

    Chapter 21 Global Variables

--- src/chunk.h
+++ src/chunk.h
@@ -10,7 +10,11 @@ typedef enum
     OP_NIL,
     OP_TRUE,
     OP_FALSE,
+    OP_POP,
     OP_EQUAL,
+    OP_GET_GLOBAL,
+    OP_DEFINE_GLOBAL,
+    OP_SET_GLOBAL,
     OP_GREATER,
     OP_LESS,
     OP_ADD,
@@ -19,6 +23,7 @@ typedef enum
     OP_DIVIDE,
     OP_NOT,
     OP_NEGATE,
+    OP_PRINT,
     OP_RETURN
 } OpCode;
 
